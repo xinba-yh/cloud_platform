c26c3bd97f43e0f4589807f33ccaec4e0c116908
Zhou Chao <602357524@qq.com>
2018-08-02 18:19:04 +0800

    eureka server changes , commit 2018年8月2日18:18:54

--- commons/src/test/java/com/qianli/ilink/cloud_platform/commons/BaseTest.java
+++ commons/src/test/java/com/qianli/ilink/cloud_platform/commons/BaseTest.java
@@ -1,18 +1,3 @@
 package com.qianli.ilink.cloud_platform.commons;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@Rollback
-public class BaseTest {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+public class BaseTest {}
